f1172349d8eff804f6979046cd649d8bdba44a7d
Hélio Meira Lins <helio@meiralins.org>
2016-04-05 19:05:30 -0300

    fixing test

--- tests/test_tapioca_twitter.py
+++ tests/test_tapioca_twitter.py
@@ -17,7 +17,7 @@ class TestTapioca(unittest.TestCase):
     def test_resource_access(self):
         resource = self.wrapper.statuses_mentions_timeline()
 
-        self.assertEqual(resource.data(), 'https://api.twitter.com/1.1/statuses/mentions_timeline.json')
+        self.assertEqual(resource.data, 'https://api.twitter.com/1.1/statuses/mentions_timeline.json')
 
 
 if __name__ == '__main__':
